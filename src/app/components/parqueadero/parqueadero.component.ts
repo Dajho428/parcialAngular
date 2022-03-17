5e900ecdbcdfbe7b9ec95e8b34ee6a05f438523d
Dajho428 <89399827+Dajho428@users.noreply.github.com>
2022-03-17 17:10:20 -0500

    Full

--- src/app/components/parqueadero/parqueadero.component.ts
+++ src/app/components/parqueadero/parqueadero.component.ts
@@ -17,14 +17,21 @@ export class ParqueaderoComponent {
     //let fechaSalida=new Date(salida);
     let fechaSistema=new Date(this.sistema);
     if(this.validarFechas(fechaIngreso,fechaSistema)==true){
+      this.horas=0;
       alert("No puedes elegir una fecha superior a la del sistema");
+
     }else{
       // alert(fechaSistema)
       this.horas=Math.abs(Math.round(((fechaSistema.getTime()-fechaIngreso.getTime())/1000)/3600)) ;
       // alert(this.horas)
-      if (this.horas>=24) return this.tarifa=10000;
+      if (this.horas==24) return this.tarifa=10000;
       else if (this.horas>3 && this.horas <24) return (this.tarifa+=(this.horas-3)*500);
-      else return this.tarifa;
+      else if (this.horas > 24) {
+        this.horas=0;
+        alert("No puede pasar de 24 horas");
+      }
+      else if(this.horas>0 && this.horas<=3)return this.tarifa;
+      else alert("Ingresa horas validas no dejes el espacio en blanco");
     }
     return this.tarifa=0;
   }
